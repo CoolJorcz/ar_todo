03084db8e9380f73e9339f4fa68ab8badaee87e2
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-02 15:30:30 -0500

    added task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,23 @@
+
 class Task < ActiveRecord::Base
+  def self.add(stuff)
+    create(description: stuff)
+  end
+
+  def self.delete(id)
+    delete_task = self.find_by_id(id)
+    delete_task.destroy
+    delete_task.save
+  end
+  
+  def self.list
+    all
+  end
+
+  def self.complete(id)
+    completed_task = self.find_by_id(id)
+    completed_task.update(complete: true)
+    completed_task.save
+  end
+
 end
