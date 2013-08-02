7fecb1bc1f63fd17d286ef0bcf1a9520dca2c637
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-02 16:10:25 -0500

    finished setting up our model

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,20 +4,17 @@ class Task < ActiveRecord::Base
     create(description: stuff)
   end
 
-  def self.delete(id)
-    delete_task = self.find_by_id(id)
-    delete_task.destroy
-    delete_task.save
+  def self.delete(list_number)
+    all[list_number].destroy
   end
   
   def self.list
     all
   end
 
-  def self.complete(id)
-    completed_task = self.find_by_id(id)
+  def self.complete(list_number)
+    completed_task = all[list_number]
     completed_task.update(complete: true)
     completed_task.save
   end
-
 end
