9f1321c6badfcdac8cd0abe8e7e30d0f3d5b2285
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-02 16:43:20 -0500

    new migration

--- todo.rb
+++ todo.rb
@@ -1,3 +1,24 @@
 require_relative 'config/application'
 
 # puts "Put your application code in #{File.expand_path(__FILE__)}"
+input = ARGV[0]
+
+case input
+
+when 'add'
+  input.shift
+
+when 'delete'
+  input.shift
+  
+    #Call controller
+    "Deleted \"#{}"
+
+  when 'list'
+
+    TaskController.list.each_index do |index|
+      "#{list_number} #{List.description[list_number]}"
+    end
+    when 'complete'
+
+    end
